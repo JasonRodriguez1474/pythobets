909f30ff61ba7bad90809ece5af3eb91d5606e9d
Jason Rodriguez <jasonrodriguez1474@gmail.com>
2022-12-28 12:57:49 -0800

    moments before I switch the project over to use click for cli

--- pythobets/main.py
+++ pythobets/main.py
@@ -14,32 +14,45 @@ app = typer.Typer()
 
 
 @app.command()
-def update_clubsoccer_predictions():
-    print("Updating Club Soccer Predictions")
-    url = "https://projects.fivethirtyeight.com/data-webpage-data/datasets/soccer-spi.zip"
+def update_predictions(sports = 'all'):
+    print("Updating Predictions")
     if not os.path.isdir(USER_DATA_PATH):
         os.makedirs(USER_DATA_PATH)
-    print("Downloading Started")    
-    req = requests.get(url)
-    print("Downloading Completed")
-    filename = url.split('/')[-1]
-    zipped_predictions = zipfile.ZipFile(BytesIO(req.content))
-    zipped_predictions.extractall(USER_DATA_PATH)
-    print("Succesfully Updated Club Soccer Predictions")
+    urls = {'nfl': 'https://projects.fivethirtyeight.com/data-webpage-data/datasets/nfl-elo.zip', 'nba': 'https://projects.fivethirtyeight.com/data-webpage-data/datasets/nba-forecasts.zip',
+            'soccer': 'https://projects.fivethirtyeight.com/data-webpage-data/datasets/soccer-spi.zip', 'nhl': 'https://projects.fivethirtyeight.com/data-webpage-data/datasets/nhl-forecasts.zip'}
+    if sports == 'all':
+        for url in urls.values():
+            current_url = url
+            filename = current_url.split('/')[-1]
+            print(f"Download Starting for {filename}") 
+            try:
+                req=requests.get(current_url)
+            except: 
+                print(f"{filename} failed to download, continuing")
+                continue   
+            zipped_predictions = zipfile.ZipFile(BytesIO(req.content))
+            zipped_predictions.extractall(USER_DATA_PATH)
+    print("Succesfully Updated Predictions")
     print("Please support https://fivethirtyeight.com/ if possible as they provide this data.")
 
 
 @app.command()
-def configure(api_key: str, betmaker: str, bankroll:int,  predictions = ['all'], staking_strategy = 'kelly-criterion'):
+def configure(the_odds_API_key: str, betmaker: str, bankroll:int,  predictions = ['all'], staking_strategy = 'kelly-criterion'):
     """
     
     """
-    configuration.ensure_config_directories
+    configuration.ensure_config_directories()
     
     print(betmaker)
-    print(api_key)
-    
+    print(the_odds_API_key)
 
+@app.command()
+def determine_upcoming_bets():
+    '''
+    Handles the automated bet selection process.
+    '''
+
+    
 
 if __name__ == "__main__":
     app()
