1b13ed1201693965e15b88f635b25a719efab132
Jason Rodriguez <jasonrodriguez1474@gmail.com>
2023-01-02 21:59:08 -0800

    added todo

--- pythobets/update_predictions.py
+++ pythobets/update_predictions.py
@@ -6,7 +6,8 @@ USER_DATA_PATH = os.path.join(os.getenv("HOME"), ".pythobets")
 USER_CONFIG_PATH = os.path.join(USER_DATA_PATH, 'config.ini')
 JSON_ODDS = os.path.join(USER_DATA_PATH, 'odds.json')
 
-
+#TODO add a sport variable that is passed to this function
+# Predictions that are run should depend on which sport was passed
 def load_predictions():
     odds = []
     todays_odds = []
@@ -15,9 +16,10 @@ def load_predictions():
     with open(SOCCER_ODDS, newline='') as csvfile:
         prediction_reader = csv.reader(csvfile, delimiter=',')
         for row in prediction_reader:
-            odds.append
+            odds.append(datetime.strptime(row[1],"%Y-%m-%d") )
             # odds.append(row[1])
-    print(datetime.strptime(odds[1], "%Y-%m-%d"))
+    # print(datetime.strptime(odds[1], "%Y-%m-%d"))
+    print(odds)
 
 
 def main():
